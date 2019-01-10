3aa8e7ea93f8367fd7a64f6ca97c4d1b93abf92c
Daniil <dmarkov@bestmood.tech>
2019-01-10 23:58:14 +0300

    -added get function -added delete function -added makeCompl function -added create function

--- api/hello/handler.ts
+++ api/hello/handler.ts
@@ -1,6 +1,12 @@
+import { errorHandler } from '../helper/error-handler';
+
 const uuid = require('uuid/v1');
 const dynamoose = require('dynamoose');
 const Schema = dynamoose.Schema;
+dynamoose.AWS.config.update({
+  region: 'us-east-1',
+});
+dynamoose.local();
 
 const todoSchema = new Schema({
   id: {
@@ -11,120 +17,51 @@ const todoSchema = new Schema({
   },
   isActive: {
     type: Boolean,
-  }
+  },
 });
-
-dynamoose.local();
 
 let Todo = dynamoose.model(process.env.USERS_TABLE, todoSchema);
 
-export function create(event, context, callback) {
+export function create(event) {
 
   const data = event.body;
 
-  if (typeof data.value !== 'string') {
+  if (!data || !data.value) {
     console.error('Validation Failed');
-    callback(null, {
-      statusCode: 400,
-      // headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\`t create the item!',
-    });
-    return;
+    return { statusCode: 400 };
   }
 
-  Todo.create({
+  return Todo.create({
     id: uuid(),
     value: data.value,
     isActive: true,
-  }, (err, todo) => {
-    if (err) {
-      console.log(err);
-      callback(null, {
-        statusCode: err.statusCode || 501,
-        body: 'Couldn\`t create the item!',
-      });
-      return;
-    }
-
-    console.log(todo.value);
-
-    const res = {
-      statusCode: 200,
-      body: JSON.stringify(todo),
-    };
-    callback(null, res);
   });
 }
 
-export function list(event, context, callback) {
-  // const params = {
-  //   TableName: process.env.USERS_TABLE,
-  // };
-  //
-  // dynamodb.scan(params, (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //     callback(null, {
-  //       statusCode: err.statusCode || 501,
-  //       // headers: { 'Content-Type:': 'text/plain' },
-  //       body: 'couldn\t fetch the item',
-  //     });
-  //     return;
-  //   }
-  //
-  //   console.log(result.Items);
-  //
-  //   const res = {
-  //     statusCode: 200,
-  //     body: JSON.stringify(result.Items),
-  //   };
-  //   callback(null, res);
-  // });
+export async function list(event) {
+  return Todo.scan().exec();
 }
 
-export function remove(event, context, callback) {
-  // const params = {
-  //   TableName: process.env.TABLE_USERS,
-  //   Key: {
-  //     id: event.pathParameters.id,
-  //   },
-  // };
-  //
-  // dynamodb.delete(params, (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     callback(null, {
-  //       statusCode: err.statusCode || 501,
-  //       // headers: { 'Content-Type': 'text/plain' },
-  //       body: 'Couldn\`t remove item',
-  //     });
-  //     return;
-  //   }
-  //
-  //   const res = {
-  //     statusCode: 200,
-  //     body: JSON.stringify({}),
-  //   };
-  //   callback(null, res);
-  // });
+export async function remove(event) {
+  const id = event.path.id;
+  console.log(id);
+  return Todo.delete({ id: id });
+
 }
 
-// export function removeAll(event, context, callback) {
-//   const params = {
-//     TableName: process.env.TABLE_USERS,
-//   };
-//
-//   dynamodb.delete(params, (err) => {
-//     if (err) {
-//       console.error(err);
-//       callback(null, {
-//         statusCode: err.statusCode || 501,
-//         // headers: { 'Content-Type': 'text/plain' },
-//         body: 'Couldn\`t remove items',
-//       });
-//       return;
-//     }
-//
-//     const res = {
-//       statusCode: 200,
-//       body: JSON.
+export function removeAll() {
+  return;
+}
+
+export function complete(event) {
+  const data = { id: event.body.data.id, value: event.body.data.value, isActive: event.body.data.isActive };
+  const todo = new Todo({ id: data.id, isActive: data.isActive, value: data.value });
+  console.log(data.isActive);
+  return todo.save()
+    .then(() => '')
+    .catch((err) => errorHandler(err));
+}
+
+export function completeAll() {
+
+}
